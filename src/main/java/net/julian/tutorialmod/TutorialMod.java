ea7919423707723a263fa5e0510882fbd76cb8e0
J <julian.a.k.rosadi@gmail.com>
2023-08-17 20:43:51 -0500

    added items

--- src/main/java/net/julian/tutorialmod/TutorialMod.java
+++ src/main/java/net/julian/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.julian.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.julian.tutorialmod.item.ModCreativeModeTabs;
+import net.julian.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,11 +31,14 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -49,7 +55,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
